ac4d3e909ec650499a6c06aa6899e3c5e5206b43
Ajinkya Patil <ajinkyapatil240499@gmail.com>
2024-11-04 00:43:24 +0530

    variables Defines

---
+++ Lectore_02/variable.js
@@ -0,0 +1,14 @@
+//! var example
+var a = 10;
+a = 20;
+console.log(a); // Output: 20
+
+//! let example
+let b = 10;
+b = 20;
+console.log(b); // Output: 20
+
+//! const example
+const c = 10;
+// c = 20; // TypeError: Assignment to constant variable.
+console.log(c); // Output: 10
