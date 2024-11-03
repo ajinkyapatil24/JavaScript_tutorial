dc0e591eb20f2507f1621c366b25e88c705cd148
Ajinkya Patil <ajinkyapatil240499@gmail.com>
2024-11-04 00:48:41 +0530

    DataTypes

---
+++ Lectore_03/script.js
@@ -0,0 +1,41 @@
+//! DataType
+
+//* 1)Primitive(7)
+//! 1)Number
+const num1=24;
+const num2=24.9;
+
+//! 2)Boolean
+const str=true;
+
+//! 3)String
+const name="ajinkya";
+
+//! 4)BigInt
+const bigint=25345879;
+
+//! 5)symbol
+const sym="ajinkya()123"
+
+//! 6)undefiend
+let age ;
+console.log(age);
+
+//! 7)null
+const num=null;
+
+//* 2)Non-Primitive
+//! Arrays
+let arr=["ajinkya",2499,7387782663,"patil"];
+
+//! Object
+let data={
+    fullname:"ajinkyapatil",
+    email:"pajinkya@gmail.com",
+    MobileNum:7387782663
+}
+
+//! function
+let myFunction=function(){
+    console.log("Hello function");
+}
